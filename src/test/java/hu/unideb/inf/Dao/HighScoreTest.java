0a7f72353cbe24e76577a3cf21a7806af82f87ff
MJ <madar94@gmail.com>
2017-05-31 20:02:55 +0200

    Add HighscoreTest

--- src/test/java/hu/unideb/inf/Dao/HighScoreTest.java
+++ src/test/java/hu/unideb/inf/Dao/HighScoreTest.java
@@ -1,7 +1,58 @@
 package hu.unideb.inf.Dao;
 
-/**
- * Created by MJ on 2017. 05. 31..
- */
+import org.junit.Before;
+import org.junit.Test;
+
+import java.time.LocalDate;
+
+import static org.junit.Assert.*;
+
 public class HighScoreTest {
+    private HighScore highScore;
+    private String date = String.valueOf(LocalDate.now());
+
+
+    @Before
+    public void setHighScore(){
+        highScore = new HighScore("Player","10", date);
+    }
+
+    @Test
+    public void testHighscore(){
+        assertNotNull(highScore);
+    }
+
+    @Test
+    public void getNameTest(){
+        assertEquals("Player",highScore.getName());
+    }
+
+    @Test
+    public void getScoreTest(){
+        assertEquals("10",highScore.getScore());
+    }
+
+    @Test
+    public void getDateTest(){
+        assertEquals(date,highScore.getDate());
+    }
+
+    @Test
+    public void setNameTest(){
+        highScore.setName("Player");
+        assertEquals("Player", highScore.getName());
+    }
+
+    @Test
+    public void setScoreTest(){
+        highScore.setScore("20");
+        assertEquals("20", highScore.getScore());
+    }
+
+    @Test
+    public void setDateTest(){
+        LocalDate date = LocalDate.of(2013,01,01);
+        highScore.setDate(String.valueOf(date));
+        assertEquals(String.valueOf(date), highScore.getDate());
+    }
 }
